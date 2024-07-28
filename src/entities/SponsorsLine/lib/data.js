9bd1d3ba77e69cbd1b8019214c0405a1ac14382d
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2024-07-28 13:13:55 -0700

    manifest and meta tags

--- src/entities/SponsorsLine/lib/data.js
+++ src/entities/SponsorsLine/lib/data.js
@@ -2,7 +2,7 @@ import sponsor1 from '../../../shared/assets/sponsors/webp/sponsor.webp';
 import sponsor2 from '../../../shared/assets/sponsors/webp/sponsor1.webp';
 import sponsor3 from '../../../shared/assets/sponsors/sponsor11.jpeg';
 import sponsor4 from '../../../shared/assets/sponsors/webp/sponsor3.webp';
-import sponsor5 from '../../../shared/assets/sponsors/webp/sponsor4.webp';
+// import sponsor5 from '../../../shared/assets/sponsors/webp/sponsor4.webp';
 import sponsor6 from '../../../shared/assets/sponsors/webp/sponsor5.webp';
 import sponsor8 from '../../../shared/assets/sponsors/webp/sponsor7.webp';
 import sponsor9 from '../../../shared/assets/sponsors/webp/sponsor8.webp';
@@ -13,6 +13,22 @@ import sponsor13 from '../../../shared/assets/sponsors/webp/sponsor12.webp';
 import sponsor14 from '../../../shared/assets/sponsors/webp/sponsor13.webp';
 import sponsor15 from '../../../shared/assets/sponsors/webp/kerl.webp';
 import sponsor16 from '../../../shared/assets/sponsors/webp/nagi.webp';
+import sponsor20 from '../../../shared/assets/sponsors/sponsor-20.jpg';
+import sponsor21 from '../../../shared/assets/sponsors/sponsor-21.jpg';
+import sponsor22 from '../../../shared/assets/sponsors/sponsor-22.jpg';
+import sponsor23 from '../../../shared/assets/sponsors/sponsor-23.jpg';
+import sponsor24 from '../../../shared/assets/sponsors/sponsor-24.jpg';
+import sponsor25 from '../../../shared/assets/sponsors/sponsor-25.jpg';
+import sponsor26 from '../../../shared/assets/sponsors/sponsor-26.jpg';
+import sponsor27 from '../../../shared/assets/sponsors/sponsor-27.jpg';
+import sponsor28 from '../../../shared/assets/sponsors/sponsor-28.jpg';
+import sponsor29 from '../../../shared/assets/sponsors/sponsor-29.jpg';
+import sponsor30 from '../../../shared/assets/sponsors/sponsor-30.jpg';
+import sponsor31 from '../../../shared/assets/sponsors/sponsor-31.jpg';
+import sponsor32 from '../../../shared/assets/sponsors/sponsor-32.jpg';
+import sponsor33 from '../../../shared/assets/sponsors/sponsor-33.jpg';
+import sponsor34 from '../../../shared/assets/sponsors/sponsor-34.jpg';
+import sponsor35 from '../../../shared/assets/sponsors/sponsor-35.jpg';
 
 
 
@@ -75,13 +91,76 @@ export const data = [
         img: sponsor11,
         name:'Bloom events',
     },
-    
-    {
-        img: sponsor5,
-        name:'Love your body coach',
-    },
+    // {
+    //     img: sponsor5,
+    //     name:'Love your body coach',
+    // },
     {
         img: sponsor6,
         name:"Aster's cake",
+    },
+    {
+        img: sponsor20,
+        name:"",
+    },
+    {
+        img: sponsor21,
+        name:"",
+    },
+    {
+        img: sponsor22,
+        name:"",
+    },
+    {
+        img: sponsor23,
+        name:"",
+    },
+    {
+        img: sponsor24,
+        name:"",
+    },
+    {
+        img: sponsor25,
+        name:"",
+    },
+    {
+        img: sponsor26,
+        name:"",
+    },
+    {
+        img: sponsor27,
+        name:"",
+    },
+    {
+        img: sponsor28,
+        name:"",
+    },
+    {
+        img: sponsor29,
+        name:"",
+    },
+    {
+        img: sponsor30,
+        name:"",
+    },
+    {
+        img: sponsor31,
+        name:"",
+    },
+    {
+        img: sponsor32,
+        name:"",
+    },
+    {
+        img: sponsor33,
+        name:"",
+    },
+    {
+        img: sponsor34,
+        name:"",
+    },
+    {
+        img: sponsor35,
+        name:"",
     },
 ]
